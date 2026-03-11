95e6ab90ebab64a93d46a6f6b1e574f4ee95a6e1
priyesh_kun <122373342+Priyesh-Kun@users.noreply.github.com>
2026-03-11 22:53:03 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CartData } from "@/types";
 import CartPageClient from "./CartPageClient";
 
 async function getCartData(): Promise<CartData> {
-  const res = await fetch("https://ecoyaan-submission-git-main-priyesh-mishras-projects.vercel.app/api/cart", {
+  const res = await fetch("http://localhost:3000/api/cart", {
     cache: "no-store",
   });
   if (!res.ok) throw new Error("Failed to fetch cart");
